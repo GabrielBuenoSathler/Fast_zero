8ccd7854a020451ce906c396ac4b964fd1a4a82c
gabriel <gabrielbuenosathler@gmail.com>
2025-12-09 17:08:37 -0500

    nova rota

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -9,8 +9,14 @@ from fast_zero.models import User,Book
 from fast_zero.security import get_password_hash,verify_password,create_access_token,get_current_user
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
-app = FastAPI()  
 
+from fast_zero.routers import users
+from fast_zero.schema import Message
+
+app = FastAPI()
+
+app.include_router(users.router)
+ 
 
 
 app.add_middleware(
@@ -24,75 +30,9 @@ app.add_middleware(
 @app.get('/')                                                                                                              
 def read_root():                                                                                                           
     return {'message': 'Olá Mundo!'}                                                                                       
-                                                                                                                           
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)                                            
-def create_user(user: UserSchema, session: Session = Depends(get_session)):                                                
-                                                                                                                           
-                                                                                                                           
-    db_user = session.scalar(                                                                                              
-        select(User).where(                                                                                                
-            (User.username == user.username) | (User.email == user.email)                                                  
-                                                                                                                           
-        )                                                                                                                  
-    )                                                                                                                      
-                                                                                                                           
-    if db_user:                                                                                                            
-        if db_user.username == user.username:                                                                              
-                                                                                                                           
-                                                                                                                           
-            raise HTTPException(                                                                                           
-                status_code=HTTPStatus.CONFLICT,                                                                           
-                detail='Username already exists',                                                                          
-            )                                                                                                              
-        elif db_user.email == user.email:                                                                                  
-            raise HTTPException(                                                                                           
-                status_code=HTTPStatus.CONFLICT,                                                                           
-                detail='Email already exists',                                                                             
-            )                                                                                                              
-                                                                                                                           
-    db_user = User(                                                                                                        
-        username=user.username, password=get_password_hash(user.password), email=user.email                                
-    )                                                                                                                      
-    session.add(db_user)                                                                                                   
-    session.commit()                                                                                                       
-    session.refresh(db_user)                                                                                               
-                                                                                                                           
-    return db_user  
+                                                                                                                                                        
+                             
 
-
-@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
-
-
-    db_user = session.scalar(
-        select(User).where(
-            (User.username == user.username) | (User.email == user.email)
-
-        )
-    )
-
-    if db_user:
-        if db_user.username == user.username:
-
-
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail='Username already exists',
-            )
-        elif db_user.email == user.email:
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail='Email already exists',
-            )
-
-    db_user = User(
-        username=user.username, password=get_password_hash(user.password), email=user.email
-    )
-    session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
-
-    return db_user
 
 @app.post('/token', response_model=Token)
 def login_for_access_token(
@@ -120,43 +60,6 @@ def login_for_access_token(
     access_token = create_access_token(data={'sub': user.email})
 
     return {'access_token': access_token, 'token_type': 'bearer'}
-
-
-@app.get('/users_teste/',status_code = HTTPStatus.OK ,response_model=UserList)
-def read_users(
-        limit : int = 10,
-        offset: int =0,
-        session: Session = Depends(get_session),
-        current_user= Depends(get_current_user),
-):
-    users = session.scalars(select(User).limit(limit).offset(offset))
-    return {'users': users}
-
-
-@app.put('/users/{user_id}', response_model=UserPublic)
-def update_user(
-    user_id: int,
-    user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
-):
-    if current_user.id != user_id:
-        raise HTTPException(
-            status_code=HTTPStatus.FORBIDDEN, detail='Not enough permissions'
-        )
-    try:
-        current_user.username = user.username
-        current_user.password = get_password_hash(user.password)
-        current_user.email = user.email
-        session.commit()
-        session.refresh(current_user)
-
-        return current_user
-    except IntegrityError:
-        raise HTTPException(
-            status_code=HTTPStatus.CONFLICT,
-            detail='Username or Email already exists',
-        )
 
 
 
